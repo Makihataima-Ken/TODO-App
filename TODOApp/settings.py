f74a1eca8c96d6ed686a243a8e81e11fa7ab50ad
Ken <ahmadsoud63@gmail.com>
2025-05-14 18:33:07 +0300

    add task model

--- TODOApp/settings.py
+++ TODOApp/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'accounts',
+    'tasks',
     'rest_framework',
     'rest_framework_simplejwt'
 ]
--- tasks/models.py
+++ tasks/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+from TODOApp import settings
+
+class task(models.Model):
+    class Status(models.TextChoices):
+        TODO = 'TD', 'To Do'
+        IN_PROGRESS = 'IP', 'In Progress'
+        DONE = 'DN', 'Done'
+    
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete= models.CASCADE)
+    title = models.CharField(max_length=200)
+    description = models.TextField(blank=True)
+    dueDate = models.DateField(blank=True)
+    # Status field using the enum
+    status = models.CharField(
+        max_length=2,
+        choices=Status.choices,
+        default=Status.TODO
+    )
+    # basic info for record
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
