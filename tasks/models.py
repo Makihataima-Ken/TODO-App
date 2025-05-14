37ad7c8fc85f7bca86ad0f07d98afa7abe107a87
Ken <ahmadsoud63@gmail.com>
2025-05-14 18:41:05 +0300

    add meta class

--- tasks/models.py
+++ tasks/models.py
@@ -22,3 +22,15 @@ class task(models.Model):
     # basic info for record
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
+    
+    def __str__(self):
+        return f"{self.title} ({self.get_status_display()})"
+    
+    class Meta:
+        ordering = ['status', '-created']  # Order by status then newest first
+        verbose_name = 'Task'
+        verbose_name_plural = 'Tasks'
+        indexes = [
+            models.Index(fields=['status']),
+            models.Index(fields=['user', 'status']),
+        ]
